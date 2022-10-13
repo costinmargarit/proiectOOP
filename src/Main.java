5720f404085a5844812e3e205691837eebc01e5d
costinmargarit <114351987+costinmargarit@users.noreply.github.com>
2022-10-13 19:31:36 +0300

    am reusit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //scriere cod pt calculator
+        System.out.println("sa calcuam niste numere");
+        System.out.println("introduce-ti primul numar fractionar");
+
+        Scanner scanner = new Scanner(System.in);
+        double numar1 = scanner.nextDouble();
+        System.out.println("introduce-ti al doilea numar fractionar");
+        double numar2 = scanner.nextDouble();
+
+    }
+}
